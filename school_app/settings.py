0892610c12c7190aa7b8fd0684c6dffbf8f25128
kartik <kartik.choubisa@gmail.com>
2024-04-16 00:16:04 +0530

    update (to test)

--- school_app/settings.py
+++ school_app/settings.py
@@ -33,10 +33,20 @@ SECRET_KEY = env.str(
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default=False)  # new
 
-ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ["https://*.fly.dev"]  
+# The `DYNO` env var is set on Heroku CI, but it's not a real Heroku app, so we have to
+# also explicitly exclude CI:
+# https://devcenter.heroku.com/articles/heroku-ci#immutable-environment-variables
+IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
+
+# SECURITY WARNING: don't run with debug turned on in production!
+if not IS_HEROKU_APP:
+    DEBUG = True
+
+if IS_HEROKU_APP:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -96,19 +106,28 @@ WSGI_APPLICATION = "school_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "SchoolDB",
-        "USER": "yogendrasankhla",
-        "PASSWORD": "",
-        "HOST": "localhost",
-        "PORT": "5432",
+if IS_HEROKU_APP:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "SchoolDB",
+            "USER": "yogendrasankhla",
+            "PASSWORD": "",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
     }
-}
+    db_from_env = dj_database_url.config(conn_max_age=600)
+    DATABASES['default'].update(db_from_env)
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+
 		
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
